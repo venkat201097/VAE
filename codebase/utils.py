6f4f47cc1979d2914991a7213de35f1c82b61b36
Venkat Krishnamohan <venkat201097@gmail.com>
2022-11-01 00:47:37 -0700

    Fixed TypeError utils:358

--- codebase/utils.py
+++ codebase/utils.py
@@ -354,9 +354,8 @@ def get_mnist_data(device, use_test_subset=True):
         xl += [X[idx][idx_choice]]
         yl += [y[idx][idx_choice]]
     xl = torch.cat(xl).to(device)
-    yl = torch.cat(yl) #.to(device)
-    yl = yl.new(np.eye(10)[yl]).to(device)
-    # yl = yl.to(device)
+    yl = torch.cat(yl).to(device)
+    yl = yl.new(np.eye(10)[yl.cpu()])
     labeled_subset = (xl, yl)
 
     return train_loader, labeled_subset, (X_test, y_test)
